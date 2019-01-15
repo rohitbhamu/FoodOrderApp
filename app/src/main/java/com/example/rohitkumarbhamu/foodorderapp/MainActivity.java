9d5a8a0896a520259b52001326f778034cb20cae
rohitbhamu <rohitbhamu6@gmail.com>
2019-01-16 01:51:46 +0530

    updated Signin.java

--- app/src/main/java/com/example/rohitkumarbhamu/foodorderapp/MainActivity.java
+++ app/src/main/java/com/example/rohitkumarbhamu/foodorderapp/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.rohitkumarbhamu.foodorderapp;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaring objects
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Intiliazing objects
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+
+        Typeface face =Typeface.createFromAsset(getAssets(),"fonts/Nabila.ttf");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
